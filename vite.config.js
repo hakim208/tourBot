613ec144e6f569b876bd5022ec6a103837412590
hakim208 <inoyatovhakim5@gmail.com>
2025-06-19 23:58:15 +0300

    IOS Mobil

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from '@tailwindcss/vite'
+import img from './public/Без названия.jpeg'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
     VitePWA({
-    registerType: "autoUpdate",
-    manifest: {
-      name: "TurboBot",
-      short_name: "TurboBot",
-      description: "Ассистенти AI барои Туризми Тоҷикистон",
-      theme_color: "#4F46E5",
-      icons: [
-        {
-          src: "my-app/public/Без названия.jpeg",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "my-app/public/Без названия.jpeg",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/api\.gemini\.example\.com\/.*/i,
-          handler: "NetworkFirst",
-          options: {
-            cacheName: "api-cache",
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 60 * 60 * 24, 
+      registerType: "autoUpdate",
+      manifest: {
+        name: "TurboBot",
+        short_name: "TurboBot",
+        description: "Ассистенти AI барои Туризми Тоҷикистон",
+        theme_color: "#4F46E5",
+        icons: [
+          {
+            src: { img },  
+            sizes: "192x192",
+            type: "image/jpeg",       
+          },
+          {
+            src: { img },
+            sizes: "512x512",
+            type: "image/jpeg",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.gemini\.example\.com\/.*/i,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "api-cache",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24, 
+              },
             },
           },
-        },
-      ],
-    },
-  }),
+        ],
+      },
+    }),
   ],
-})
+});
