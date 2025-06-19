8d79eaab77a283278f8e759aacacc185ed7ce1cc
hakim208 <inoyatovhakim5@gmail.com>
2025-06-19 23:38:03 +0300

    Mobil

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "TurboBot",
+      short_name: "TurboBot",
+      description: "Ассистенти AI барои Туризми Тоҷикистон",
+      theme_color: "#4F46E5",
+      icons: [
+        {
+          src: "my-app/public/Без названия.jpeg",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "my-app/public/Без названия.jpeg",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/api\.gemini\.example\.com\/.*/i,
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api-cache",
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24, 
+            },
+          },
+        },
+      ],
+    },
+  }),
+  ],
 })
